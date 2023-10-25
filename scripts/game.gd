45092716c6189fcb06e2a1e33a34dfc8c50fc89c
twincannon <twincannon@gmail.com>
2023-10-24 22:19:13 -0400

    ui theme, cursor, trees

--- scripts/game.gd
+++ scripts/game.gd
@@ -79,6 +79,7 @@ func on_viewport_changed():
 	var viewport_size = get_viewport().size
 	var scaled = Vector2(viewport_size) / ((tile_size * Vector2(grid_size)) + padding)
 	$GridAnchor.scale = Vector2(min(scaled.x, scaled.y),min(scaled.x, scaled.y))
+	$Background.position.x = viewport_size.x/2
 	
 	#%TutorialContainer.add_theme_constant_override("margin_right", 20 if viewport_size.x < 1000 else 300)
 	#%TutorialContainer.add_theme_constant_override("margin_left", 20 if viewport_size.x < 1000 else 300)
