twincannon/GemSlide

commit 5f1e02b4ed5f2a7e3ba90f89da8b99cc4a8a7069

teleporter ice fix and more

scripts/entities/black_gem.gd
@@ -10,8 +10,10 @@ func _does_block(_other_entity):
 	return true
 
 func _on_movement(_dir):
+	super(_dir)
 	#This is copypasted from Gem... gross
 	if movement_tween:
+		movement_tween.stop()
 		movement_tween.kill()
 	movement_tween = create_tween().set_parallel(true)
 	var offset = (Vector2(_dir.x, _dir.y) * distance_to_move)

scripts/entities/entity.gd
@@ -10,8 +10,12 @@ var movement_tween_queue_time := 0.2
 var grid_pos:Vector2i : set = set_grid_pos # Position on game grid
 @onready var distance_to_move := Globals.get_entity_movement_distance() # I want this to be const
 
+var queued_teleport_pos:Vector2i = Vector2i(-1,-1)
+
 var entity_sprite:Sprite2D # Sprite for moving, rotating intended to be at 0,0 with 0 rotation by default
 var entity_id := 0
+
+var moving := false
 
 var moves := false # If this entity moves when input is pressed
 var is_forcibly_moving := false
@@ -29,6 +33,17 @@ func set_grid_pos(new_pos:Vector2i):
 
 func _entity_pre_move(dir:Vector2i):
 	on_entity_pre_move.emit(self, dir)
+
+func _entity_post_move():
+	if queued_teleport_pos != Vector2i(-1,-1):
+		for e in Globals.get_game_node().get_entities_at_pos(queued_teleport_pos):
+			if !(e is Teleporter):
+				e.teleport_to(grid_pos)
+	
+		teleport_to(queued_teleport_pos)
+		
+		queued_teleport_pos = Vector2i(-1,-1)
+
 
 func can_move_in_dir(dir:Vector2i, ignore_stuck = false) -> bool:
 	var game_node := Globals.get_game_node() as Game
@@ -75,6 +90,7 @@ func _should_increment_moves(dir):
 	return stuck and moves and can_move_in_dir(dir, true)# Should take into account "can ever un-stuck" somehow. Basically this is hardcoded for sandtraps atm
 
 func _on_movement(_dir):
+	moving = true
 	# Do move tween here, make sure to call _on_movement_tween_done via tween callback!
 	
 	#does this make sense? I kind of hate inheritance for this
@@ -94,17 +110,31 @@ func _on_entity_entered(_other_entity):
 
 func _on_entity_finished_entering(_other_entity):
 	pass
+
+func _on_entity_finished_exiting(_other_entity):
+	pass
 	
 func _does_block(_other_entity):
 	return true
 
 func _on_movement_tween_done(dir):
+	moving = false
 	teleport_to(grid_pos)
+	for e in Globals.get_game_node().get_entities_at_pos(grid_pos - dir):
+		e._on_entity_finished_exiting(self)
 	for e in Globals.get_game_node().get_entities_at_pos(grid_pos):
 		e._on_entity_finished_entering(self)
 	if is_forcibly_moving:
 		if !on_try_move(dir):
 			is_forcibly_moving = false
+	if !is_forcibly_moving:
+		on_movement_done.emit(self)
+			
+func queue_teleport_to(pos:Vector2i):
+	queued_teleport_pos = pos
+
+func fake_teleport_to(pos:Vector2i):
+	position = Globals.get_game_node().get_position_at_grid_pos(pos)
 
 func teleport_to(pos:Vector2i):
 	if movement_tween and movement_tween.is_valid():

scripts/entities/gem.gd
@@ -21,6 +21,7 @@ func _process(_delta):
 	$Control/LabelGridPos.text = str(grid_pos.x, ",", grid_pos.y)
 
 func _on_movement(_dir):
+	super(_dir)
 	if movement_tween:
 		movement_tween.stop()
 		movement_tween.kill()

scripts/entities/ice_slick.gd
@@ -7,8 +7,13 @@ func _ready():
 func _does_block(_other_entity):
 	return false
 
-func _on_entity_exited(_other_entity):
+#func _on_entity_exited(_other_entity):
+func _on_entity_finished_exiting(_other_entity):
 	_other_entity.is_forcibly_moving = false
+	
 
 func _on_entity_entered(_other_entity):
+	_other_entity.is_forcibly_moving = true
+	
+func _on_entity_finished_entering(_other_entity):
 	_other_entity.is_forcibly_moving = true

scripts/entities/teleporter.gd
@@ -33,6 +33,7 @@ func find_next_teleporter():
 				lowest_tele = e
 	return lowest_tele
 
+#_on_entity_entered works here so we just need to do the queued teleport after this
 func _on_entity_finished_entering(_other_entity):
 	var other_teleporter = find_next_teleporter()
 	
@@ -40,9 +41,9 @@ func _on_entity_finished_entering(_other_entity):
 		printerr("Failed to find destination teleporter")
 		return
 	
-	for e in Globals.get_game_node().get_entities_at_pos(other_teleporter.grid_pos):
-		if !(e is Teleporter):
-			e.teleport_to(grid_pos)
+#	for e in Globals.get_game_node().get_entities_at_pos(other_teleporter.grid_pos):
+#		if !(e is Teleporter):
+#			e.teleport_to(grid_pos)
 	
 	var vfx = teleport_vfx_scene.instantiate()
 	add_child(vfx)
@@ -51,4 +52,7 @@ func _on_entity_finished_entering(_other_entity):
 
 	$Audio.play()
 	
-	_other_entity.teleport_to(other_teleporter.grid_pos)
+	# We do a fake teleport here as well as the real one (which is queued) in _entity_post_move so that we can visibly instantly teleport when delayed by ice movement etc.
+	_other_entity.fake_teleport_to(other_teleporter.grid_pos)
+	
+	_other_entity.queue_teleport_to(other_teleporter.grid_pos)

scripts/game.gd
@@ -137,7 +137,7 @@ func _process(_delta):
 		entities.erase(e)
 	entities_to_remove.clear()
 	
-	if !move_cooldown_timer.is_stopped(): return	
+	if !move_cooldown_timer.is_stopped(): return
 	var all_goals_filled = true
 	for e in entities:
 		var goal = e as Goal
@@ -230,13 +230,29 @@ func move_entities(dir:Vector2i):
 	#	for i in range(entities.size() - 1, -1, -1):
 	#		if entities[i].on_try_move(dir) or entities[i]._should_increment_moves(dir):
 	#			did_any_entity_move = true
-	
+		
 	if did_any_entity_move:
 		$Audio/MoveAudioPlayer.stream = move_sounds[randi() % move_sounds.size()]
 		$Audio/MoveAudioPlayer.play()
 		increment_moves()
 		moves_array.append(dir)
+		for e in entities:
+			if e.moving:
+				e.on_movement_done.connect(check_for_last_movement)
 
+func check_for_last_movement(entity:Entity):
+	entity.on_movement_done.disconnect(check_for_last_movement)
+	var any_entity_moving = false
+	for e in entities:
+		if e.moving:
+			any_entity_moving = true
+	if !any_entity_moving:
+		on_all_movement_finished()
+
+func on_all_movement_finished():
+	print("Test")
+	for i in entities:
+		i._entity_post_move()
 
 func increment_moves():
 	moves += 1

commit 6aeeaf5c028f6d44a61b9cb25ae433f6683a0666

fix nasty ice slick bug that was introduced in 5f1e02b

scenes/entities/black_gem.tscn
@@ -18,3 +18,25 @@ z_index = 100
 texture = ExtResource("2_ruqie")
 
 [node name="ColorblindHelper" type="Sprite2D" parent="."]
+
+[node name="Control" type="Control" parent="."]
+visible = false
+z_index = 999
+layout_mode = 3
+anchors_preset = 0
+offset_top = -32.0
+offset_right = 40.0
+offset_bottom = 8.0
+
+[node name="LabelGridPos" type="Label" parent="Control"]
+layout_mode = 1
+anchors_preset = -1
+anchor_left = -1.575
+anchor_top = -1.575
+anchor_right = 1.6
+anchor_bottom = -0.425
+grow_horizontal = 2
+theme_override_font_sizes/font_size = 36
+text = "0,0"
+horizontal_alignment = 1
+metadata/_edit_use_anchors_ = true

scripts/entities/black_gem.gd
@@ -5,6 +5,9 @@ func _ready():
 	entity_sprite = %GemSprite
 	moves = true
 	%GemSpriteRotAnchor.rotation = randf_range(-PI, PI)
+	
+#func _process(_delta):
+#	$Control/LabelGridPos.text = str(grid_pos.x, ",", grid_pos.y)
 
 func _does_block(_other_entity):
 	return true

scripts/entities/entity.gd
@@ -34,7 +34,7 @@ func set_grid_pos(new_pos:Vector2i):
 func _entity_pre_move(dir:Vector2i):
 	on_entity_pre_move.emit(self, dir)
 
-func _entity_post_move():
+func _entity_post_all_movement():
 	if queued_teleport_pos != Vector2i(-1,-1):
 		for e in Globals.get_game_node().get_entities_at_pos(queued_teleport_pos):
 			if !(e is Teleporter):
@@ -125,9 +125,11 @@ func _on_movement_tween_done(dir):
 	for e in Globals.get_game_node().get_entities_at_pos(grid_pos):
 		e._on_entity_finished_entering(self)
 	if is_forcibly_moving:
-		if !on_try_move(dir):
+		if on_try_move(dir):
+			moving = true
+		else:
 			is_forcibly_moving = false
-	if !is_forcibly_moving:
+	if !moving:
 		on_movement_done.emit(self)
 			
 func queue_teleport_to(pos:Vector2i):

scripts/entities/gem.gd
@@ -17,14 +17,15 @@ func _ready():
 	%GemSpriteRotAnchor.rotation = randf_range(-PI, PI)
 	add_to_group("gems")
 
-func _process(_delta):
-	$Control/LabelGridPos.text = str(grid_pos.x, ",", grid_pos.y)
+#func _process(_delta):
+#	$Control/LabelGridPos.text = str(grid_pos.x, ",", grid_pos.y)
 
 func _on_movement(_dir):
 	super(_dir)
 	if movement_tween:
 		movement_tween.stop()
 		movement_tween.kill()
+	
 	movement_tween = create_tween().set_parallel(true)
 	#movement_tween.set_ease(Tween.EASE_OUT)
 	#movement_tween.set_trans(Tween.TRANS_ELASTIC)

scripts/entities/ice_slick.gd
@@ -7,13 +7,12 @@ func _ready():
 func _does_block(_other_entity):
 	return false
 
-#func _on_entity_exited(_other_entity):
-func _on_entity_finished_exiting(_other_entity):
+func _on_entity_exited(_other_entity):
 	_other_entity.is_forcibly_moving = false
 	
 
 func _on_entity_entered(_other_entity):
 	_other_entity.is_forcibly_moving = true
 	
-func _on_entity_finished_entering(_other_entity):
-	_other_entity.is_forcibly_moving = true
+#func _on_entity_finished_entering(_other_entity):
+#	_other_entity.is_forcibly_moving = true

scripts/entities/teleporter.gd
@@ -52,7 +52,7 @@ func _on_entity_finished_entering(_other_entity):
 
 	$Audio.play()
 	
-	# We do a fake teleport here as well as the real one (which is queued) in _entity_post_move so that we can visibly instantly teleport when delayed by ice movement etc.
+	# We do a fake teleport here as well as the real one (which is queued) in _entity_post_all_movement so that we can visibly instantly teleport when delayed by ice movement etc.
 	_other_entity.fake_teleport_to(other_teleporter.grid_pos)
 	
 	_other_entity.queue_teleport_to(other_teleporter.grid_pos)

scripts/game.gd
@@ -252,7 +252,7 @@ func check_for_last_movement(entity:Entity):
 
 func on_all_movement_finished():
 	for i in entities:
-		i._entity_post_move()
+		i._entity_post_all_movement()
 
 func increment_moves():
 	moves += 1